e27a0b7aec2f3a2f7d4c45be50375814f7cf9130
vansh <vansh2124@gmail.com>
2024-04-04 01:32:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pixelperfect/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.pixelperfect;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button yesButton = findViewById(R.id.yesButton);
+        yesButton.setBackgroundColor(Color.parseColor("#70c734"));
+
+        Button noButton = findViewById(R.id.noButton);
+        noButton.setBackgroundColor(Color.parseColor("#f1d682"));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
